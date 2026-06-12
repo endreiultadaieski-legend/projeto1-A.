e773bcc0910b05c922d0bb054d60412a2801ce2d
endreiul <endreiul.tadaieski@escola.pr.gov.br>
2026-06-12 08:10:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+<button onclick="abrirTradutor()">Abrir Tradutor</button>
 
+<script>
+function abrirTradutor() {
+    window.location.href = "https://www.google.com/search?q=tradutor&sourceid=chrome&ie=UTF-8";
+}
+</script>
